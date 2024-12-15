8ed0b7198fca3492394ebc535d1a4443c494956e
teppei1204 <m-n-m09@docomo.ne.jp>
2024-12-15 22:54:33 +0900

    itemモデル、マイグレーションファイルの削除

--- db/schema.rb
+++ db/schema.rb
@@ -11,28 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2024_12_12_155312) do
-  create_table "items", charset: "utf8mb3", force: :cascade do |t|
-    t.string "name", null: false
-    t.integer "price", null: false
-    t.text "description", null: false
-    t.integer "shipping_cost_id", null: false
-    t.integer "shipping_day_id", null: false
-    t.integer "shipping_id", null: false
-    t.bigint "user_id", null: false
-    t.integer "prefecture_id", null: false
-    t.integer "condition_id", null: false
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_items_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_items_on_reset_password_token", unique: true
-    t.index ["user_id"], name: "index_items_on_user_id"
-  end
-
   create_table "users", charset: "utf8mb3", force: :cascade do |t|
     t.string "nick_name", null: false
     t.string "first_name", null: false
@@ -49,5 +27,4 @@ ActiveRecord::Schema[7.0].define(version: 2024_12_12_155312) do
     t.datetime "updated_at", null: false
   end
 
-  add_foreign_key "items", "users"
 end
