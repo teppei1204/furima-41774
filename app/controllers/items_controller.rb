293ff5fc244622aa12d34b336d65c1d9a178bdfb
teppei1204 <m-n-m09@docomo.ne.jp>
2024-12-19 21:07:21 +0900

    「未ログインユーザー：ログインページ」「出品者以外のログインユーザー：トップページ」へ遷移

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :correct_user, only: [:edit, :update]
   def index
     @items = Item.order(created_at: :desc)
   end
@@ -22,11 +23,31 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path(@item.id)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def item_params
     params.require(:item).permit(:name, :description, :price, :shipping_cost_id, :shipping_day_id, :category_id, :prefecture_id,
                                  :condition_id, :image)
           .merge(user_id: current_user.id)
+  end
+
+  def correct_user
+    @item = Item.find(params[:id])
+    return if current_user.id == @item.user.id
+
+    redirect_to root_path
   end
 end
