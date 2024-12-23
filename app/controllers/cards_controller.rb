a5d150108c1688dc42ddca22f93caf79f429e13d
teppei1204 <m-n-m09@docomo.ne.jp>
2024-12-23 15:29:20 +0900

    ログイン中購入画面に遷移できる

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,4 +1,30 @@
 class CardsController < ApplicationController
   def index
+    @item = Item.find(params[:item_id])
+    @card_form = CardForm.new
+  end
+
+  def new
+    @item = Item.find(params[:item_id])
+    @card_form = CardForm.new
+  end
+
+  def create
+    @item = Item.find(params[:item_id])
+    @card_form = CardForm.new(card_form_params)
+    if @card_form.valid?
+      @card_form.save
+      redirect_to root_path
+    else
+      render :index, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def card_form_params
+    params.require(:card_form).permit(:post_code, :prefecture_id, :city, :address, :building_name, :phone_number).merge(
+      user_id: current_user.id, item_id: params[:item_id]
+    )
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   root to: "items#index"
-  resources :items
-
-  resources :cards, only: :index
+  resources :items do
+      resources :cards, only: [:index, :new, :create]
+  end
 end
